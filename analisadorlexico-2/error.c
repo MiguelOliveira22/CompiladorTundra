d9147f42b08d9aff42152c4140ceb462e822a71d
cc24143 <miguelqueiroz360@gmail.com>
2026-05-15 13:45:25 -0300

    AAAAAAAAA

---
+++ analisadorlexico-2/error.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "error.h"
+
+void sairErro(FILE* file, int codigoErro, char* tipoErro) {
+    fclose(file);
+    printf(tipoErro);
+    
+    exit(codigoErro);
+}

f3b2e55473e14cd6655769d3fd0fa3d052215116
cc24143 <miguelqueiroz360@gmail.com>
2026-05-22 11:56:13 -0300

    eu comito, tu comitas, nos comitamos

---
+++ analisadorlexico-3/analisadortools.h
@@ -0,0 +1,68 @@
+#ifndef analisadortools
+#define analisadortools
+
+#include "basics.h"
+
+extern const i8 MAX_LENGTH_LEXICO;
+
+typedef enum {
+    programa,
+    rotulo,
+    tipo,
+    variavel,
+    procedimento,
+    funcao,
+    inicio,
+    fim,
+    atribuicao,
+    se,
+    entao,
+    senao,
+    enquanto,
+    faca,
+    vapara,
+    mais,
+    menos,
+    vezes,
+    dividir,
+    igual,
+    diferente,
+    menor,
+    menorouigual,
+    maior,
+    maiorouigual,
+    e,
+    ou,
+    nao,
+    abreparenteses,
+    fechaparenteses,
+    abrecolchetes,
+    fechacolchetes,
+    virgula,
+    pontoevirgula,
+    doispontos,
+    ponto,
+    abrecomentario,
+    fechacomentario,
+    
+    invalido,
+    eof,
+    
+    null,
+    identificador,
+    numero,
+} CodigoToken;
+
+typedef struct {
+    CodigoToken codigoToken;
+    char*       lexicoArquivo;
+    bool        isEspecial;
+    char*       nomeCompilador;
+} token;
+
+extern const token tokens[];
+
+token analex(FILE* file);
+void  anasin(FILE* file);
+
+#endif

d32a2ab8a9ea5a4bd9a7dbfb62a26714c9d389ce
MatheusFranco2008 <162757230+cc24609@users.noreply.github.com>
2026-05-24 23:59:22 -0300

    alteração final

--- compiladorsi/main.c
+++ compiladorsi/main.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "lista.h"
 #include "analisadortools.h"
 #include "error.h"
-
-int comparaBosta(Elemento a, Elemento b) {
-    int valorA = *(int*)a;
-    int valorB = *(int*)b;
-    
-    if (valorA < valorB) return -1;
-    if (valorA > valorB) return 1;
-    return 0;
-}
 
 int main(int argc, char* argv[])
 {
